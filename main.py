6278c415e89316e4d297b279137911ad9991736e
Your Name <you@example.com>
2020-09-13 11:34:43 +0900

    first

---
+++ main.py
@@ -0,0 +1,12 @@
+import settings
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(asctime)s :%(message)s', level=logging.DEBUG)
+
+
+def main():
+    logging.info('hoge')
+
+
+if __name__ == '__main__':
+    main()
