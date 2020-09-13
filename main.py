0423259f8ffc3079ccddbdb3e996b0c2582b1548
Hideyoshi Mikata <hideyoshi.mikata@music.yamaha.com>
2020-09-13 17:36:50 +0900

    mod

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import settings
 import logging
-
+from Controller.api import OandaApi
+from Model.pricing import test
 logging.basicConfig(format='%(levelname)s:%(asctime)s :%(message)s', level=logging.DEBUG)
 
 
 def main():
     logging.info('hoge')
+
+    o = OandaApi(settings.oanda_token, settings.oanda_id)
+    # res = o.summary()
+    # print(res)
+
+    #print(o.pricing())
+    #print(o.order())
+    #print(o.ordering())
+
+    test()
 
 
 if __name__ == '__main__':
